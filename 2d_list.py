3586b1de59c3018e38a8043cdc4632efd18c71f3
takeuchisatoshi <satsitt3@gmail.com>
2019-03-09 15:56:52 +0900

    二次元リストの勉強

---
+++ 2d_list.py
@@ -0,0 +1,12 @@
+users = [["Bob", 11],
+         ["Tom", 34],
+         ["Ken", 65]]
+
+# user = users[0]
+# print(f"{user[0]}さんは{user[1]}歳です")
+
+print(f"{users[0][0]}さんは{users[0][1]}歳です")
+
+print(f"{users[1][0]}さんは{users[1][1]}歳です")
+
+print(f"{users[2][0]}さんは{users[2][1]}歳です")
