0d1fa92b4648a506c843f53f3ed8c868ea7f7836
takeuchisatoshi <satsitt3@gmail.com>
2019-03-09 16:07:59 +0900

    for構文の勉強

--- string_list.py
+++ string_list.py
@@ -1,5 +1,8 @@
 members = ["Bob", "Tom", "Ken"]
 
-print(members[2])
-print(members[1])
-print(members[0])
+# print(members[0])
+# print(members[1])
+# print(members[2])
+
+for member in members:
+    print(member)
